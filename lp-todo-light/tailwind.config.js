602f84c88c004f924df5483bd7167fe50e3d2d57
PK-Yakkun <mokkun515@gmail.com>
2023-04-22 23:50:04 +0900

    feat: theme更新

--- lp-todo-light/tailwind.config.js
+++ lp-todo-light/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          primary: "#0047ff",
+          secondary: "#4200FF",
+          text: "#0081f9",
+        },
+        purple: "#5f00bd",
+      },
+      backgroundImage: {
+        "mv-bg": "url(/images/mv-bg.jpg)",
+      },
+    },
   },
   plugins: [],
 };
