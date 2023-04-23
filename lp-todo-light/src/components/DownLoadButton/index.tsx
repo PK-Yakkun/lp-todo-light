73464414c129061bb8eecda05a0142391c8f34f1
PK-Yakkun <mokkun515@gmail.com>
2023-04-23 10:20:27 +0900

    feat: DownloadButton, H1改修

--- lp-todo-light/src/components/DownLoadButton/index.tsx
+++ lp-todo-light/src/components/DownLoadButton/index.tsx
@@ -2,7 +2,7 @@ import DownloadIcon from "@/components/icons/Download";
 
 const DownLoadButton = () => {
   return (
-    <button className="flex items-center gap-2 bg-gradient-to-r from-purple to-blue-primary text-white font-bold py-3 px-5 rounded-md">
+    <button className="flex items-center gap-2 bg-gradient-to-r from-purple to-blue-primary text-white font-bold text-xl py-5 px-12 rounded-lg shadow-[0_8px_20px_0px_#5f00bd66] transition duration-300 hover:opacity-80">
       Download for Mac
       <DownloadIcon />
     </button>
--- lp-todo-light/src/components/H1/index.tsx
+++ lp-todo-light/src/components/H1/index.tsx
@@ -1,6 +1,6 @@
 const H1 = () => {
   return (
-    <h1 className="inline-block bg-clip-text text-transparent leading-loose text-6xl font-bold bg-gradient-to-r from-purple to-blue-primary">
+    <h1 className="inline-block bg-clip-text text-transparent leading-loose text-6xl font-bold bg-gradient-to-r from-[#0081F9] from-0% via-[#9B5EFF] via-60% to-[#02BEEC] to-100%">
       Todo Light
     </h1>
   );
